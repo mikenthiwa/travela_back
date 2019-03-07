4f64ea79b370a7ca9b186ae017a2b5ef99d91899
Uchechukwu Akogwu <34472497+ucheg6@users.noreply.github.com>
2019-03-07 21:42:24 +0100

    #164476151 fix the permission restriction for checklist (#402)
    fix(checklist): fix the permission restriction for checklist
     - add requester roleId to the allowed permissions
    
    [Fixes #164476151]

--- src/middlewares/RoleValidator.js
+++ src/middlewares/RoleValidator.js
@@ -163,7 +163,7 @@ export default class RoleValidator {
     const { destinationName } = req.query;
     const { email } = req.user.UserInfo;
     if (destinationName) {
-      const allowedPermissions = [29187, 10948, 339458];
+      const allowedPermissions = [29187, 10948, 339458, 401938];
       const hasPermission = await RoleValidator
         .checkUserRoleById(email, allowedPermissions, res, next);
       /* istanbul ignore next */

92d22d15de3672b38e037a310a91ff81516f89c7
Dee Kerubo <34271684+Deekerubo@users.noreply.github.com>
2019-06-01 08:05:47 +0300

    feat(user migration): data migration (#479)
    - ETL to create users from bamboo api and save users bamboo id to database
    
    [Delivers #165238294]

---
+++ migrator/databaseConnection.js
@@ -0,0 +1,8 @@
+// create a database connection
+const pg = require('pg');
+
+require('dotenv').config();
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+
+module.exports = pool;

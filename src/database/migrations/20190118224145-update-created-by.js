8c411a0172e8bf6039fd6e55ba034ad2190d4a9c
Moses Gitau <gitaumoses4@gmail.com>
2019-07-19 10:07:16 +0300

    TRAV-125-bug(migrations): fix the issue with foreign key implementation (#579)
    - ensure the sequence of the queries in the migration is enhanced
    
    [Fixes TRAV-125]

--- src/database/migrations/20190118224145-update-created-by.js
+++ src/database/migrations/20190118224145-update-created-by.js
@@ -1,18 +1,18 @@
 module.exports = {
-  up: queryInterface => Promise.all([
-    queryInterface.removeConstraint(
+  up: async (queryInterface) => {
+    await queryInterface.removeConstraint(
       'ReminderEmailTemplates', 'ReminderEmailTemplates_createdBy_fkey'
-    ),
-    queryInterface.sequelize.query(
+    );
+    await queryInterface.sequelize.query(
       `ALTER TABLE "ReminderEmailTemplates" alter column "createdBy" 
           TYPE INTEGER USING "createdBy"::integer`
-    ),
-    queryInterface.sequelize.query(
+    );
+    await queryInterface.sequelize.query(
       `ALTER TABLE "ReminderEmailTemplates" ADD CONSTRAINT "ReminderEmailTemplates_createdBy_fkey"
       FOREIGN KEY ("createdBy") REFERENCES "Users"(id) 
       ON DELETE CASCADE;`
-    )
-  ]),
+    );
+  },
   down: queryInterface => queryInterface.removeConstraint(
     'ReminderEmailTemplates', 'ReminderEmailTemplates_createdBy_fkey'
   ),
